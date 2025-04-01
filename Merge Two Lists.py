9cbf2c09db2f745f65518f1b3ea5dcf59a99de15
yash-9182 <yeshu9182@gmail.com>
2025-04-01 15:31:25 +0530

    Create  Merge Two Lists.py

---
+++ Merge Two Lists.py
@@ -0,0 +1,5 @@
+def merge_lists(lst1, lst2):
+    print("Merging lists:", lst1, "and", lst2)
+    return lst1 + lst2
+print("Merged List:", merge_lists([1, 2, 3], [4, 5, 6]))
+print("---------------------------")
