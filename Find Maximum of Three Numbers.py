61ade3649dfa20e0ee7738681b923acb0bc3bcd1
yash-9182 <yeshu9182@gmail.com>
2025-04-01 15:23:20 +0530

    Create Find Maximum of Three Numbers.py

---
+++ Find Maximum of Three Numbers.py
@@ -0,0 +1,7 @@
+def max_of_three(a, b, c):
+    print("Finding max of:", a, b, c)
+    maximum = max(a, b, c)
+    return maximum
+print("Max:", max_of_three(3, 7, 5))
+print("Max found.")
+print("---------------------------")
