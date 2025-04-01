ccebabfb291e5685cfd284e0153dff46eda7cabb
yash-9182 <yeshu9182@gmail.com>
2025-04-01 15:22:44 +0530

    Create Reverse a String.py

---
+++ Reverse a String.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    print("Original string:", s)
+    reversed_s = s[::-1]
+    return reversed_s
+text = "Python"
+reversed_text = reverse_string(text)
+print("Reversed:", reversed_text)
+print("String reversed.")
+print("---------------------------")
