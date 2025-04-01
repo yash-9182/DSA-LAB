c3d3ef77e1394740c6c4d2b566887fefeba7767d
yash-9182 <yeshu9182@gmail.com>
2025-04-01 15:20:23 +0530

    Create  Sum of Two Numbers.py

---
+++ Sum of Two Numbers.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    result = a + b
+    print("Adding:", a, "+", b, "=", result)
+    return result
+x, y = 5, 10
+sum_result = add_numbers(x, y)
+print("Sum:", sum_result)
+print("Calculation done.")
+print("---------------------------")
