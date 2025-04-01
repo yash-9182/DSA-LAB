7a5fcefac70c2ca5a13cbbdf63e9d4a86215bc2f
yash-9182 <yeshu9182@gmail.com>
2025-04-01 15:21:09 +0530

    Create Check Even or Odd.py

---
+++ Check Even or Odd.py
@@ -0,0 +1,11 @@
+def check_even_odd(num):
+    print("Checking number:", num)
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+n = 7
+result = check_even_odd(n)
+print("Result:", result)
+print("Check completed.")
+print("---------------------------")
